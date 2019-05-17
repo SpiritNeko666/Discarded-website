21faf3a3d448701bcdc75cdc0c5e83b410f746ac
SpiritNeko666 <50715412+SpiritNeko666@users.noreply.github.com>
2019-05-17 16:09:51 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,6 +31,33 @@ function greet() {
   }
 
 function draw() {
+ background(220);
+   
+  
+  stroke('black')
+  
+  ellipseMode(RADIANS)
+  
+  ellipse(195,220,10,30)
+ellipse(205,220,10,30)
+  ellipse(193,200,10,30)
+  
 
+  
+  ellipse(207,200,10,30)
+  
+ 
+ push()
+  noStroke()
+    ellipse(200,200,10,30)
+  pop()
+  ellipse(200,180,20,20)
+  ellipse(203.5,180,5,5)
+  ellipse(196.5,180,5,5)
+   
+
+
+  
+  
 }
 }
