002c86b7d183423ef533913aeb0a99b9931abb7b
SpiritNeko666 <50715412+SpiritNeko666@users.noreply.github.com>
2019-05-17 11:09:39 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function setup() {
   button.position(input.x + input.width, 65);
   button.mousePressed(greet);
   
-  greeting = createElement('h2', 'what is your name?');
+  greeting = createElement('h2', 'username');
   greeting.position(20, 5);
 
   textAlign(CENTER);
@@ -18,17 +18,25 @@ function setup() {
   textSize(15);
   noStroke();
   }
-  function greet() {
+  
+function greet() {
   const name = input.value();
-  greeting.html('hello ' + name + '!');
+  greeting.html('Welcome ' + name + ' to creature city');
   input.value('');
 
   for (let i = 0; i < 200; i++) {
     push();
-    fill(random(255), 255, 255);
-    translate(random(width), random(height));
-    rotate(random(2 * PI));
-    text(name, 0, 0);
+    
     pop();
   }
-  }
+
+function draw() {
+  //const r = rSlider.value();
+ // const g = gSlider.value();
+  //const b = bSlider.value();
+  //background(r, g, b);
+ // text('red', rSlider.x + 50 + rSlider.width, 35);
+  //text('green', gSlider.x + 50 + gSlider.width, 65);
+  //text('blue', bSlider.x + 50 + bSlider.width, 95);
+}
+}
