0f80b3b57a0d593aed29bc1b36b029b1c03bb23e
SpiritNeko666 <50715412+SpiritNeko666@users.noreply.github.com>
2019-05-18 12:38:38 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,37 +1,34 @@
+let input, button, greeting;
 function setup() {
-  createCanvas(400, 400);
- 
-}
+ createCanvas(710, 400);
 
-function draw() {
-   
+  input = createInput();
+  input.position(20, 65);
+   button = createButton('submit');
+  button.position(input.x + input.width, 65);
+  button.mousePressed(greet);
   
-  
-  
-  ellipseMode(RADIANS)
-  
-  ellipse(195,220,10,30)
-ellipse(205,220,10,30)
-  ellipse(193,200,10,30)
-  
+  greeting = createElement('h2', 'username');
+  greeting.position(20, 5);
 
-  
-  ellipse(207,200,10,30)
+  textAlign(CENTER);
+  textSize(50);
   
  
- push()
-  noStroke()
-    ellipse(200,200,10,30)
-  pop()
-  ellipse(200,180,20,20)
-  ellipse(203.5,180,5,5)
-  ellipse(196.5,180,5,5)
-   
-
-
+  textSize(15);
+  noStroke();
+  }
   
-  
+function greet() {
+  const name = input.value();
+  greeting.html('Welcome ' + name + ' to creature city');
+  input.value('');
 
-  
+  for (let i = 0; i < 200; i++) {
+    push();
+    
+    pop();
+  }
+
   
 }
