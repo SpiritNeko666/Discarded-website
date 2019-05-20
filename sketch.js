425b88f6cef7ce1e80a117ac90f496fb64879691
SpiritNeko666 <50715412+SpiritNeko666@users.noreply.github.com>
2019-05-19 19:56:46 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,41 +1,47 @@
- let spirit;
-let souls  = []
+let input, button, greeting;
 function setup() {
-createCanvas(600, 400);
-  for (let i = 0; i  < 2; i++) {
-  souls[i] = new Soul(300,200);
+ createCanvas(710, 400);
+background(200,200,200)
+  input = createInput();
+  input.position(20, 65);
+   button = createButton('submit');
+  button.position(input.x + input.width, 65);
+  button.mousePressed(greet);
+  
+  greeting = createElement('h2', 'username');
+  greeting.position(20, 5);
+
+  textAlign(CENTER);
+  textSize(50);
+  
+ 
+  textSize(15);
+  noStroke();
   }
-}
+  
+function greet() {
+  const name = input.value();
+  greeting.html('Welcome ' + name + ' to creature city');
+  input.value('');
+  
+  for (let i = 0; i < 200; i++) {
+    push();
+  
 
-function draw() {
-background(255);
-    for (let i = 0; i  < 2; i++){
 
-souls[i].move();
-souls[i].show();
-    }
-}
-
-class Soul {
-  constructor() {
-    this.x = 300;
-    this.y = 200;
-  }
-
-move() {
-this.x = this.x 
-this.y = this.y 
-}
-
-show() {
-
-beginShape()  
+ beginShape()  
 push()
+  stroke('black')
+    this.x = 300
+    this.y = 200
+    push()
   ellipse(this.x,this.y+20, 10,30)
     ellipse(this.x+15,this.y+10, 35,8)
     ellipse(this.x-15,this.y+10, 35,8)
     ellipse(this.x,this.y+40, 10,35)
 
+  ellipse(this.x,this.y ,20,20)
+  push()
   ellipse(this.x,this.y ,20,20)
   push()
   fill(255, 255,0)
@@ -44,7 +50,9 @@ push()
 pop()
   line(this.x+3,this.y+1 ,this.x+3, this.y-1.5 )
   line(this.x-4,this.y +1,this.x-4, this.y-1.5 )
-}
+  
+   pop();
+  }
 
-
+  
 }
