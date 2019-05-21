8d53fac8d861abec9e73b1a9d4b9235a467ad758
SpiritNeko666 <50715412+SpiritNeko666@users.noreply.github.com>
2019-05-21 11:20:13 -0700

    Update login.js

--- login.js
+++ login.js
@@ -1,66 +1,37 @@
 let input, button, greeting;
-function setup() {
- createCanvas(1710, 1400);
-background(200,200,200)
-  input = createInput();
-  input.position(20, 65);
-   button = createButton('submit');
-  button.position(input.x + input.width, 65);
-  button.mousePressed(greet);
-  
-  greeting = createElement('h2', 'username');
-  greeting.position(20, 5);
 
+function setup() {
+  // create canvas
+  createCanvas(710, 400);
+
+  username = createInput();
+  username.position(150, 65);
+  password = createInput();
+  password.position(150, 165);
+  button = createButton('submit');
+
+  button.position(username.x + username.width, 265);
+  button.mousePressed(greet);
+  button.mousePressed(greet);
+  greeting = createElement('h2', 'Username');
+  greeting.position(20, 40);
+  greeting = createElement('h2', 'Password');
+  greeting.position(20, 145);
   textAlign(CENTER);
   textSize(50);
-  
- 
-  textSize(15);
-  noStroke();
-  }
-  
+}
+
 function greet() {
   const name = input.value();
-  greeting.html('Welcome ' + name + ' to creature city');
+  greeting.html('hello ' + name + '!');
   input.value('');
-  
+
   for (let i = 0; i < 200; i++) {
     push();
-  
-
-
- beginShape()  
-push()
-  stroke('black')
-    this.x = 300
-    this.y = 200
-    push()
-    strokeJoin(ROUND);
-
-  ellipse(this.x,this.y+20, 10,30)
-    ellipse(this.x+15,this.y+10, 35,8)
-    ellipse(this.x-15,this.y+10, 35,8)
-    ellipse(this.x,this.y+40, 10,35)
-    
-      ellipse(this.x+5,this.y-8 ,10,10)
-      ellipse(this.x-5,this.y-8 ,10,10)
-
-  ellipse(this.x,this.y ,20,20)
-  push()
-  ellipse(this.x,this.y ,20,20)
-  ellipse(this.x,this.y+6 ,2,2)
-
-  push()
-  fill(255, 255,0)
-  ellipse(this.x+3.5,this.y,5,5)
-  ellipse(this.x-3.5,this.y ,5,5)
-    
-pop()
-  line(this.x+3,this.y+1 ,this.x+3, this.y-1.5 )
-  line(this.x-4,this.y +1,this.x-4, this.y-1.5 )
-  
-   pop();
+    fill(random(255), 255, 255);
+    translate(random(width), random(height));
+    rotate(random(2 * PI));
+    text(name, 0, 0);
+    pop();
   }
-
-  
 }
