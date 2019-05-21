f545544564b898fd9afb2c6fb6e1f70e6a63501c
SpiritNeko666 <50715412+SpiritNeko666@users.noreply.github.com>
2019-05-21 11:37:45 -0700

    Update login.js

--- login.js
+++ login.js
@@ -10,28 +10,22 @@ function setup() {
   password.position(150, 165);
   button = createButton('submit');
 
-  button.position(username.x + username.width, 265);
   button.mousePressed(greet);
   button.mousePressed(greet);
+  passcode = createElement('h2', 'Password');
+  passcode.position(20, 140);
   greeting = createElement('h2', 'Username');
   greeting.position(20, 40);
-  greeting = createElement('h2', 'Password');
-  greeting.position(20, 145);
   textAlign(CENTER);
   textSize(50);
 }
 
 function greet() {
   const name = username.value();
+  const code = password.value();
   greeting.html('hello ' + name + '!');
   username.value('');
+  password.value('');
 
-  for (let i = 0; i < 200; i++) {
-    push();
-    fill(random(255), 255, 255);
-    translate(random(width), random(height));
-    rotate(random(2 * PI));
-    text(name, 0, 0);
-    pop();
-  }
+  
 }
