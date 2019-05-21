2918aaa7a5f73dd5d07cb72071ca328d7b4dafdd
SpiritNeko666 <50715412+SpiritNeko666@users.noreply.github.com>
2019-05-21 11:25:23 -0700

    Create signup.js

---
+++ signup.js
@@ -0,0 +1,37 @@
+let input, button, greeting;
+
+function setup() {
+  // create canvas
+  createCanvas(710, 400);
+
+  username = createInput();
+  username.position(150, 65);
+  password = createInput();
+  password.position(150, 165);
+  button = createButton('submit');
+
+  button.position(username.x + username.width, 265);
+  button.mousePressed(greet);
+  button.mousePressed(greet);
+  greeting = createElement('h2', 'Username');
+  greeting.position(20, 40);
+  greeting = createElement('h2', 'Password');
+  greeting.position(20, 145);
+  textAlign(CENTER);
+  textSize(50);
+}
+
+function greet() {
+  const name = username.value();
+  greeting.html('hello ' + name + '!');
+  username.value('');
+
+  for (let i = 0; i < 200; i++) {
+    push();
+    fill(random(255), 255, 255);
+    translate(random(width), random(height));
+    rotate(random(2 * PI));
+    text(name, 0, 0);
+    pop();
+  }
+}
