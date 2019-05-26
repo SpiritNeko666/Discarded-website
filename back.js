6ac9921d916ee154d2e38223e7db1639d10bfd66
SpiritNeko666 <50715412+SpiritNeko666@users.noreply.github.com>
2019-05-26 15:30:41 -0700

    Update back.js

--- back.js
+++ back.js
@@ -1,1 +1,19 @@
 
+pos_y = 5
+function setup() {
+  createCanvas(400, 400);
+  neko = new Neko(100,20);
+}
+
+function draw() {
+  neko.show()
+
+}
+
+function mouseWheel(event) {
+  //move the square according to the vertical scroll amount
+
+  pos_y += event.delta;
+  //uncomment to block page scrolling
+  return(false)
+}
