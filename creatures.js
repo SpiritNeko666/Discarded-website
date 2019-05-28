da9479241df6f9cf913bc6fe746415b43c26959a
SpiritNeko666 <50715412+SpiritNeko666@users.noreply.github.com>
2019-05-28 11:09:14 -0700

    Update creatures.js

--- creatures.js
+++ creatures.js
@@ -4,4 +4,9 @@ function setup() {
 
 function draw() {
   background(220);
+textSize(16);
+textAlign(RIGHT);
+text('You do not have a creature yet', 250, 30);
+textAlign(CENTER);
+text('press New Game to create one', 250, 50);
 }
