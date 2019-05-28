830588f875e32b193b180bb59971428f27338f27
SpiritNeko666 <50715412+SpiritNeko666@users.noreply.github.com>
2019-05-28 11:36:57 -0700

    Update new_game.js

--- new_game.js
+++ new_game.js
@@ -1,27 +1,6 @@
 var sel;
-
 function setup() {
   textAlign(CENTER);
   createCanvas(400,400)
-  background(200)
-  sel = createSelect();
-  button = createButton('Continue');
-  button.position(100,10)
-  button.mousePressed(Continue);
-  sel.position(10, 10);
-  sel.option(' Creature 1');
-  sel.option(' Creature 2');
-  sel.option(' Creature 3');
-  sel.changed(mySelectEvent);
 }
-function Continue() {
-  
-  remove();
-  }
-function mySelectEvent() {
-  
-  var item = sel.value();
-  background(200);
-  text('Press Continue to Choose' + item + '', 50, 50);
-  
-}
+
